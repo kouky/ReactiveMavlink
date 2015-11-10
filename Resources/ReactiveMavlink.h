b25c3b996595b183236c10342eb69887c5e5a188
Michael Koukoullis <mike@kouky.org>
2015-11-10 23:04:12 +1100

    Create OSX target

--- Resources/ReactiveMavlink.h
+++ Resources/ReactiveMavlink.h
@@ -6,14 +6,6 @@
 //  Copyright © 2015 Michael Koukoullis. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for ReactiveMavlink.
+#import <Foundation/Foundation.h>
 FOUNDATION_EXPORT double ReactiveMavlinkVersionNumber;
-
-//! Project version string for ReactiveMavlink.
 FOUNDATION_EXPORT const unsigned char ReactiveMavlinkVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ReactiveMavlink/PublicHeader.h>
-
-
