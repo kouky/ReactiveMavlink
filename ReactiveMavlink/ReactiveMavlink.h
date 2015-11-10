657804c8c0f5bd4fc7707f32484ad3e58f092ab9
Michael Koukoullis <mike@kouky.org>
2015-11-10 19:17:39 +1100

    Initial ios framework project

---
+++ ReactiveMavlink/ReactiveMavlink.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveMavlink.h
+//  ReactiveMavlink
+//
+//  Created by Michael Koukoullis on 10/11/2015.
+//  Copyright © 2015 Michael Koukoullis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveMavlink.
+FOUNDATION_EXPORT double ReactiveMavlinkVersionNumber;
+
+//! Project version string for ReactiveMavlink.
+FOUNDATION_EXPORT const unsigned char ReactiveMavlinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveMavlink/PublicHeader.h>
+
+
